a1b8198c74de260d3bfcf953688270254880aa4c
Tiago095 <tiago2005fafe@gmail.com>
2026-03-25 15:46:58 +0000

    Correções pós primeiro teste

--- Sensor/Program.cs
+++ Sensor/Program.cs
@@ -18,80 +18,80 @@ namespace Sensor
             Console.Write("ID do Sensor: ");
             string id = Console.ReadLine();
 
-            byte[] buffer_msg = new byte[1024];
-            int bytesRead_msg = stream.Read(buffer_msg, 0, buffer_msg.Length);
+            string first_msg = $"HELLO;{id}";
 
-            string resposta1 = Encoding.UTF8.GetString(buffer_msg, 0, bytesRead_msg);
-            Console.WriteLine("Gateway respondeu: " + resposta1);
+            byte[] data_first = Encoding.UTF8.GetBytes(first_msg);
+            stream.Write(data_first, 0, data_first.Length);
 
-            while (true)
+            byte[] buffer_first = new byte[1024];
+            int bytesRead_first = stream.Read(buffer_first, 0, buffer_first.Length);
+            string resposta_first = Encoding.UTF8.GetString(buffer_first, 0, bytesRead_first);
+
+            if (resposta_first == "OK")
             {
-                Console.WriteLine("\n1 - HELLO");
-                Console.WriteLine("2 - Enviar tipos");
-                Console.WriteLine("3 - Enviar dados");
-                Console.WriteLine("4 - Heartbeat");
-                Console.WriteLine("5 - Disconnect");
+                Console.WriteLine("Ligação estabelecida com sucesso!");
 
-                string op = Console.ReadLine();
-                string msg = "";
-
-                switch (op)
+                while (true)
                 {
-                    case "1":
-                        msg = $"HELLO;{id}";
+                    Console.WriteLine("\n1 - Enviar tipos");
+                    Console.WriteLine("2 - Enviar dados");
+                    Console.WriteLine("3 - Heartbeat");
+                    Console.WriteLine("4 - Disconnect");
+
+                    string op = Console.ReadLine();
+                    string msg = "";
+
+                    switch (op)
+                    {
+                        case "1":
+                            msg = "TEMP;HUM;RUIDO";
+                            break;
+                        case "2":
+                            msg = $"{DateTime.Now:yyyy-MM-ddTHH:mm:ss};{id};ZONA_ESCOLAR;TEMP;23";
+                            break;
+                        case "3":
+                            msg = $"HEARTBEAT;{id}";
+                            break;
+                        case "4":
+                            msg = "DISCONNECT";
+                            break;
+                    }
+
+                    byte[] data = Encoding.UTF8.GetBytes(msg);
+                    stream.Write(data, 0, data.Length);
+
+                    byte[] buffer = new byte[1024];
+                    int bytesRead = stream.Read(buffer, 0, buffer.Length);
+
+                    string resposta = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    Console.WriteLine("Gateway respondeu: " + resposta);
+
+                    // lógica baseada na resposta
+                    if (resposta == "TYPES_OK")
+                    {
+                        Console.WriteLine("Tipos de dados aceites.");
+                    }
+                    else if (resposta == "DATA_RECEIVED")
+                    {
+                        Console.WriteLine("Dados enviados com sucesso.");
+                    }
+                    else if (resposta == "HEARTBEAT_OK")
+                    {
+                        Console.WriteLine("Sensor ativo confirmado.");
+                    }
+                    else if (resposta == "BYE")
+                    {
+                        Console.WriteLine("Ligação terminada.");
                         break;
-                    case "2":
-                        msg = "TEMP;HUM;RUIDO";
-                        break;
-                    case "3":
-                        msg = $"{DateTime.Now:yyyy-MM-ddTHH:mm:ss};{id};ZONA_ESCOLAR;TEMP;23";
-                        break;
-                    case "4":
-                        msg = $"HEARTBEAT;{id}";
-                        break;
-                    case "5":
-                        msg = "DISCONNECT";
-                        break;
+                    }
+                    else if (resposta.StartsWith("ERROR"))
+                    {
+                        Console.WriteLine("Erro do gateway: " + resposta);
+                    }
                 }
-
-                byte[] data = Encoding.UTF8.GetBytes(msg);
-                stream.Write(data, 0, data.Length);
-
-                byte[] buffer = new byte[1024];
-                int bytesRead = stream.Read(buffer, 0, buffer.Length);
-
-                string resposta = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-                Console.WriteLine("Gateway respondeu: " + resposta);
-
-                // lógica baseada na resposta
-                if (resposta == "OK")
-                {
-                    Console.WriteLine("Ligação estabelecida com sucesso!");
-                }
-                else if (resposta == "TYPES_OK")
-                {
-                    Console.WriteLine("Tipos de dados aceites.");
-                }
-                else if (resposta == "DATA_RECEIVED")
-                {
-                    Console.WriteLine("Dados enviados com sucesso.");
-                }
-                else if (resposta == "HEARTBEAT_OK")
-                {
-                    Console.WriteLine("Sensor ativo confirmado.");
-                }
-                else if (resposta == "BYE")
-                {
-                    Console.WriteLine("Ligação terminada.");
-                    break;
-                }
-                else if (resposta.StartsWith("ERROR"))
-                {
-                    Console.WriteLine("Erro do gateway: " + resposta);
-                }
-
-                client.Close();
             }
+
+            client.Close();
         }
     }
 }
