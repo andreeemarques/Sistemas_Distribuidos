b3f08ade33045afd0842112b2987ae544380ff9e
Tiago095 <tiago2005fafe@gmail.com>
2026-04-02 17:14:22 +0100

    Fase 3 - Inicio, envio autonomo de 1 em 1 min de valores aleatórios e heartbeat. (Tipos de dados ainda são predefenidos)

--- Sensor/Program.cs
+++ Sensor/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Sensor
@@ -20,78 +21,59 @@ namespace Sensor
 
             string first_msg = $"HELLO;{id}";
 
-            byte[] data_first = Encoding.UTF8.GetBytes(first_msg);
-            stream.Write(data_first, 0, data_first.Length);
+            Send(stream, $"HELLO;{id}");
+            string resposta = Receive(stream);
 
-            byte[] buffer_first = new byte[1024];
-            int bytesRead_first = stream.Read(buffer_first, 0, buffer_first.Length);
-            string resposta_first = Encoding.UTF8.GetString(buffer_first, 0, bytesRead_first);
-
-            if (resposta_first == "OK")
+            if (resposta != "OK")
             {
-                Console.WriteLine("Ligação estabelecida com sucesso!");
-
-                while (true)
-                {
-                    Console.WriteLine("\n1 - Enviar tipos");
-                    Console.WriteLine("2 - Enviar dados");
-                    Console.WriteLine("3 - Heartbeat");
-                    Console.WriteLine("4 - Disconnect");
-
-                    string op = Console.ReadLine();
-                    string msg = "";
-
-                    switch (op)
-                    {
-                        case "1":
-                            msg = "TEMP;HUM;RUIDO";
-                            break;
-                        case "2":
-                            msg = $"{DateTime.Now:yyyy-MM-ddTHH:mm:ss};{id};ZONA_ESCOLAR;TEMP;23";
-                            break;
-                        case "3":
-                            msg = $"HEARTBEAT;{id}";
-                            break;
-                        case "4":
-                            msg = "DISCONNECT";
-                            break;
-                    }
-
-                    byte[] data = Encoding.UTF8.GetBytes(msg);
-                    stream.Write(data, 0, data.Length);
-
-                    byte[] buffer = new byte[1024];
-                    int bytesRead = stream.Read(buffer, 0, buffer.Length);
-
-                    string resposta = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-                    Console.WriteLine("Gateway respondeu: " + resposta);
-
-                    // lógica baseada na resposta
-                    if (resposta == "TYPES_OK")
-                    {
-                        Console.WriteLine("Tipos de dados aceites.");
-                    }
-                    else if (resposta == "DATA_RECEIVED")
-                    {
-                        Console.WriteLine("Dados enviados com sucesso.");
-                    }
-                    else if (resposta == "HEARTBEAT_OK")
-                    {
-                        Console.WriteLine("Sensor ativo confirmado.");
-                    }
-                    else if (resposta == "BYE")
-                    {
-                        Console.WriteLine("Ligação terminada.");
-                        break;
-                    }
-                    else if (resposta.StartsWith("ERROR"))
-                    {
-                        Console.WriteLine("Erro do gateway: " + resposta);
-                    }
-                }
+                Console.WriteLine("Erro na ligação.");
+                return;
             }
 
-            client.Close();
+            Console.WriteLine("Ligado ao Gateway!");
+
+            Send(stream, "TEMP;HUM;RUIDO");
+            Console.WriteLine("Tipos -> " + Receive(stream));
+
+            Random rnd = new Random();
+
+            while (true)
+            {
+                int temp = rnd.Next(15, 35);
+                int hum = rnd.Next(30, 90);
+                int ruido = rnd.Next(40, 100);
+
+                string timestamp = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss");
+
+                string msgTemp = $"{timestamp};{id};ZONA_ESCOLAR;TEMP;{temp}";
+                Send(stream, msgTemp);
+                Console.WriteLine("TEMP -> " + Receive(stream));
+
+                string msgHum = $"{timestamp};{id};ZONA_ESCOLAR;HUM;{hum}";
+                Send(stream, msgHum);
+                Console.WriteLine("HUM -> " + Receive(stream));
+
+                string msgRuido = $"{timestamp};{id};ZONA_ESCOLAR;RUIDO;{ruido}";
+                Send(stream, msgRuido);
+                Console.WriteLine("RUIDO -> " + Receive(stream));
+
+                Send(stream, $"HEARTBEAT;{id}");
+                Console.WriteLine("Heartbeat -> " + Receive(stream));
+
+                Thread.Sleep(60000);
+            }
+        }
+        static void Send(NetworkStream stream, string msg)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(msg);
+            stream.Write(data, 0, data.Length);
+        }
+
+        static string Receive(NetworkStream stream)
+        {
+            byte[] buffer = new byte[1024];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            return Encoding.UTF8.GetString(buffer, 0, bytesRead);
         }
     }
 }
