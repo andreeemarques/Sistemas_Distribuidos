b95dbef6eddb975032aeadb7f74a6972e7516d47
Tiago095 <tiago2005fafe@gmail.com>
2026-04-09 22:48:36 +0100

    Criação das listas de parametros e zonas , automização do envio de parametros, dados e heartbeat.

--- Sensor/Program.cs
+++ Sensor/Program.cs
@@ -1,68 +1,151 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace Sensor
 {
     internal class Program
     {
+        static readonly List<string> Parametros = new List<string>
+        {
+            "TEMP", "HUM", "RUIDO", "AR", "PM2.5", "PM10", "LUM"
+        };
+
+        static readonly List<string> Zonas = new List<string>
+        {
+            "ZONA_ESCOLAR", "ZONA_CENTRO", "ZONA_INDUSTRIAL",
+            "ZONA_RESIDENCIAL", "ZONA_PARQUE", "ZONA_AVENIDA"
+        };
+
+        static int GerarValor(string parametro, Random rnd)
+        {
+            switch (parametro)
+            {
+                case "TEMP":
+                    return rnd.Next(15, 40);
+
+                case "HUM":
+                    return rnd.Next(30, 95);
+
+                case "RUIDO":
+                    return rnd.Next(40, 110);
+
+                case "AR":
+                    return rnd.Next(0, 200);
+
+                case "PM2.5":
+                    return rnd.Next(0, 150);
+
+                case "PM10":
+                    return rnd.Next(0, 200);
+
+                case "LUM":
+                    return rnd.Next(0, 1000);
+
+                default:
+                    return rnd.Next(0, 100);
+            }
+        }
+
         static void Main(string[] args)
         {
             TcpClient client = new TcpClient("127.0.0.1", 5000);
             var stream = client.GetStream();
 
+            DateTime ultimoHeartbeat = DateTime.Now;
+
             Console.Write("ID do Sensor: ");
             string id = Console.ReadLine();
-
-            string first_msg = $"HELLO;{id}";
 
             Send(stream, $"HELLO;{id}");
             string resposta = Receive(stream);
 
-            if (resposta != "OK")
+            if (string.Compare(resposta, "OK") != 0)
             {
-                Console.WriteLine("Erro na ligação.");
+                Console.WriteLine("Erro na ligação: " + resposta);
                 return;
             }
 
             Console.WriteLine("Ligado ao Gateway!");
 
-            Send(stream, "TEMP;HUM;RUIDO");
-            Console.WriteLine("Tipos -> " + Receive(stream));
-
             Random rnd = new Random();
 
             while (true)
             {
-                int temp = rnd.Next(15, 35);
-                int hum = rnd.Next(30, 90);
-                int ruido = rnd.Next(40, 100);
+                if ((DateTime.Now - ultimoHeartbeat).TotalMinutes >= 2)
+                {
+                    Send(stream, $"HEARTBEAT;{id}");
+                    Console.WriteLine("Heartbeat -> " + Receive(stream));
 
-                string timestamp = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss");
+                    ultimoHeartbeat = DateTime.Now;
+                }
+                else
+                {
+                    int numParametros = rnd.Next(2, Parametros.Count + 1);
 
-                string msgTemp = $"{timestamp};{id};ZONA_ESCOLAR;TEMP;{temp}";
-                Send(stream, msgTemp);
-                Console.WriteLine("TEMP -> " + Receive(stream));
+                    List<string> parametrosSelecionados = new List<string>(Parametros);
+                    for (int i = parametrosSelecionados.Count - 1; i > 0; i--)
+                    {
+                        int j = rnd.Next(i + 1);
+                        (parametrosSelecionados[i], parametrosSelecionados[j]) =
+                            (parametrosSelecionados[j], parametrosSelecionados[i]);
+                    }
+                    parametrosSelecionados = parametrosSelecionados.GetRange(0, numParametros);
 
-                string msgHum = $"{timestamp};{id};ZONA_ESCOLAR;HUM;{hum}";
-                Send(stream, msgHum);
-                Console.WriteLine("HUM -> " + Receive(stream));
+                    string tiposMsg = string.Join(";", parametrosSelecionados);
+                    Send(stream, tiposMsg);
+                    string conf_tipos = Receive(stream);
 
-                string msgRuido = $"{timestamp};{id};ZONA_ESCOLAR;RUIDO;{ruido}";
-                Send(stream, msgRuido);
-                Console.WriteLine("RUIDO -> " + Receive(stream));
+                    if (string.Compare(conf_tipos, "TYPES_OK") == 0)
+                    {
+                        Console.WriteLine("Tipos registados -> " + conf_tipos);
+                        Console.WriteLine($"Parâmetros activos ({numParametros}): {tiposMsg}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Erro:{conf_tipos}");
+                        break;
+                    }
 
-                Send(stream, $"HEARTBEAT;{id}");
-                Console.WriteLine("Heartbeat -> " + Receive(stream));
+                    string zona = Zonas[rnd.Next(Zonas.Count)];
+                    string timestamp = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss");
+
+                    foreach (string parametro in parametrosSelecionados)
+                    {
+                        int valor = GerarValor(parametro, rnd);
+                        string msg = $"{timestamp};{id};{zona};{parametro};{valor}";
+
+                        Send(stream, msg);
+                        string confirmacao = Receive(stream);
+
+                        if (string.Compare(confirmacao, "DATA_RECEIVED") == 0)
+                            Console.WriteLine($"{parametro}={valor} - DATA_RECEIVED");
+                        else
+                        {
+                            Console.WriteLine($"Erro:{confirmacao} - {parametro}");
+                            break;
+                        }
+                    }
+
+                    if ((DateTime.Now - ultimoHeartbeat).TotalMinutes >= 2.3)
+                    {
+                        Send(stream, $"HEARTBEAT;{id}");
+                        Console.WriteLine("Heartbeat -> " + Receive(stream));
+
+                        ultimoHeartbeat = DateTime.Now;
+                    }
+                }
 
                 Thread.Sleep(60000);
             }
+
+            Send(stream, "DISCONNECT");
+            Console.WriteLine(Receive(stream));
+            client.Close();
         }
+
         static void Send(NetworkStream stream, string msg)
         {
             byte[] data = Encoding.UTF8.GetBytes(msg);
