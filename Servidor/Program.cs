e98ca164070a4717c6f804c2f02b02ad35c29f57
Pedro-Vieira555 <al81302@alunos.utad.pt>
2026-04-10 12:01:11 +0100

    Servidor corrigido para os dados

--- Servidor/Program.cs
+++ Servidor/Program.cs
@@ -7,7 +7,7 @@ using System.Threading;
 
 class Servidor
 {
-    static readonly object fileLock = new object();
+    static Mutex mutex = new Mutex();
 
     static void Main()
     {
@@ -26,37 +26,27 @@ class Servidor
     static void HandleClient(TcpClient client)
     {
         NetworkStream stream = client.GetStream();
-        StringBuilder sb = new StringBuilder();
         byte[] buffer = new byte[1024];
 
         try
         {
-            // Lê todo o conteúdo enviado pelo gateway
-            int bytesRead;
-            while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
-            {
-                string parte = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-                sb.Append(parte);
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string data = Encoding.UTF8.GetString(buffer, 0, bytesRead).Trim();
+            Console.WriteLine("Recebido: " + data);
 
-                // O gateway envia "END" no final para indicar que terminou
-                if (sb.ToString().Contains("END"))
-                    break;
+            // Mutex garante que apenas um gateway escreve no ficheiro de cada vez
+            mutex.WaitOne();
+            try
+            {
+                File.AppendAllText("dados_recebidos.txt", data + Environment.NewLine);
+                Console.WriteLine("Guardado em dados_recebidos.txt");
+            }
+            finally
+            {
+                mutex.ReleaseMutex();
             }
 
-            // Remove a marca "END" do conteúdo
-            string conteudo = sb.ToString().Replace("END", "").Trim();
-            Console.WriteLine("Ficheiro recebido:\n" + conteudo);
-
-            // Guarda o conteúdo num ficheiro local com lock
-            lock (fileLock)
-            {
-                string nomeFicheiro = "dados_recebidos.txt";
-                File.AppendAllText(nomeFicheiro, conteudo + Environment.NewLine);
-                Console.WriteLine("Guardado em " + nomeFicheiro);
-            }
-
-            // Responde ao gateway
-            byte[] resposta = Encoding.UTF8.GetBytes("FILE_STORED\r\n");
+            byte[] resposta = Encoding.UTF8.GetBytes("DATA_STORED\r\n");
             stream.Write(resposta, 0, resposta.Length);
         }
         catch (Exception ex)
